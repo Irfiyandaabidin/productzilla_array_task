5b224c78a28ecf5c4e32f24df295ef3ae439a3cc
Irfiyandaabidin <irfidupang@gmail.com>
2023-03-07 20:51:32 +0700

    first commit

---
+++ array.js
@@ -0,0 +1,18 @@
+let wadah1 = ['sapu', 'pel', 'ember', 'gayung', 'ember lagi', 'gayung lagi'];
+let wadah2 = ['apel', 'pisang', 'jambu', 'buah naga', 'jeruk', 'anggur'];
+let wadah3 = ['kucing', 'semut', 'anjing', 'cacing', 'ayam', 'cicak'];
+
+console.log('======= Wadah 1 =======')
+console.log(wadah1[1]);
+console.log(wadah1[3]);
+console.log(wadah1[5]);
+
+console.log('======= Wadah 2 =======')
+console.log(wadah2[1]);
+console.log(wadah2[3]);
+console.log(wadah2[5]);
+
+console.log('======= Wadah 3 =======')
+console.log(wadah3[1]);
+console.log(wadah3[3]);
+console.log(wadah3[5]);
