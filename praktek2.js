daff1d5c27ae6799b6bf4c90320485325f429d98
Irfiyandaabidin <irfidupang@gmail.com>
2023-03-07 21:09:57 +0700

    second commit

---
+++ praktek2.js
@@ -0,0 +1,17 @@
+let arrayMulti = [
+    ['sapu', 'pel', 'ember', 'gayung', 'ember lagi', 'gayung lagi'],
+    ['apel', 'pisang', 'jambu', 'buah naga', 'jeruk', 'anggur'],
+    ['kucing', 'semut', 'anjing', 'cacing', 'ayam', 'cicak']
+];
+
+console.log('======= arrayMulti ambil genap =======')
+console.log(arrayMulti[0][2]);
+console.log(arrayMulti[0][4]);
+
+console.log('=======')
+console.log(arrayMulti[1][2]);
+console.log(arrayMulti[1][4]);
+
+console.log('=======')
+console.log(arrayMulti[2][2]);
+console.log(arrayMulti[2][4]);
